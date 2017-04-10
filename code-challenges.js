dda0df7bb10d995b8a20fe9aa766896975f9f2f1
Leslye Lu <Leslye@Leslyes-MacBook-Pro.local>
2017-04-10 17:21:48 +0200

    Refactor Decode Constructor to Code, and Add decode() and encode() methods.

--- code-challenges.js
+++ code-challenges.js
@@ -36,27 +36,84 @@ PigLatin.prototype.isPigLatin = function(){
 
 // ===============  Decode =================
 
-var Decode = function(code) {
+var Code = function(code) {
     this.code = code;
 };
 
-Decode.prototype.isDecoded = function(){
+Code.prototype.decode = function(){
 
     var message = '';
 
-    for (var i=0; i < this.code.length; i++){
+    for (var i=0; i < this.code.length; ){
         if (/[0-9]/.test(this.code[i])) {
-            message += this.code[i + 1 + parseInt(this.code[i])];
+            var spacing = parseInt(this.code[i]);
+            message += this.code[i + 1 + spacing];
+            i += spacing + 2;
+        } else {
+            i++;
         }
     }
 
     return message;
 };
 
+Code.prototype.encode = function(){
+
+    var message = '';
+
+    for (var i = 0; i < this.code.length; i++) {
+        var k = Math.floor(Math.random() * 5);
+        message += k;
+
+        for (var j = 0; j < k; j++) {
+            var spacingLetter = String.fromCharCode(97 + Math.floor(Math.random() * 26));
+            message += spacingLetter;
+        }
+
+        message += this.code[i];
+    }
+
+    this.code = message;
+    console.log(this.code);
+    return this;
+
+
+};
+
+// ===============  Leaping Lemur =================
+// A lemur wants to jump across a span in the forest on branches. She can jump 1 or 2 branches at a time. Unfortunately, some of the branches are on dead trees, and she can’t use those branches to jump.
+// 0 represents alive branches and 1 represents dead branches. The lemur starts on the first branch in the list (index 0) and is finished when she reaches the last branch in the list.
+
+// Write a function that returns the number of jumps needed:
+
+var LeapingLemur = function(branches){
+    this.branches = branches;
+};
+
+LeapingLemur.prototype.jumps = function(){
+
+    totalJumps = 0;
+
+    for (var i = 0; i < this.branches.length; i++){
+        if (this.branches[i + 1] === 0 && this.branches[i + 2] === 0) {
+            totalJumps += 1;
+            i += 2;
+        } else if (this.branches[i + 1] === 0 && this.branches[i + 2] === 1) {
+            totalJumps += 1;
+            i += 1;
+
+        }
+    }
+
+    return totalJumps;
+};
+
+
 
 // Key is Palindrome, value is the Constructor/function Palindrome.
 module.exports = {
     Palindrome: Palindrome,
     PigLatin: PigLatin,
-    Decode: Decode
+    Code: Code,
+    LeapingLemur: LeapingLemur
 };
--- code-challenges.spec.js
+++ code-challenges.spec.js
@@ -49,21 +49,69 @@ describe('Turn phrase into a PigLatin', function(){
 // ===================================================
 // ===============  Decode Tests =====================
 
-describe('Decode the following phrase', function(){
+describe('Decode or encode the following string', function(){
     
-    it('Decodes a Word', function() {
-        var decode = new codeChallenges.Decode('0h1ae2bcy');
-        expect(decode.isDecoded()).toEqual('hey');
+    it('Decodes a Word starting with a Number', function() {
+        var decode = new codeChallenges.Code('0h1ae2bcy');
+        expect(decode.decode()).toEqual('hey');
+    });
+
+    it('Decodes a Word starting with Letters', function() {
+        var decode = new codeChallenges.Code('hnd0h1ae2bcy');
+        expect(decode.decode()).toEqual('hey');
     });
 
     it('Decodes a Word with Uppercase Letters', function() {
-        var decode = new codeChallenges.Decode('3bkdB0a2jhl2okl1fo2oso0n');
-        expect(decode.isDecoded()).toEqual('Balloon');
+        var decode = new codeChallenges.Code('3bkdB0a2jhl2okl1fo2oso0n');
+        expect(decode.decode()).toEqual('Balloon');
     });
 
     it('Decodes a Phrase', function() {
-        var decode = new codeChallenges.Decode('0I 2cdh1a3abcv2ace 13 2kld1o2adg1s0.');
-        expect(decode.isDecoded()).toEqual('I have 3 dogs.');
+        var decode = new codeChallenges.Code('2mmI0 4lqyqh0a3vubv2ope1n 3dcs31f 3lywd3ceao4rldrg3svus3bjb.');
+        expect(decode.decode()).toEqual('I have 3 dogs.');
+    });
+
+    it('Encodes a String', function() {
+        var encode = new codeChallenges.Code('I have 3 dogs.');
+        expect(encode.encode().decode()).toEqual('I have 3 dogs.');
+    });
+
+
+});
+
+// ===================================================
+// ===============  Leaping Lemur =====================
+
+describe('How many jumps does the Lemur need to jump', function(){
+    
+    it('No Branches', function() {
+        var lemur = new codeChallenges.LeapingLemur([0]);
+        expect(lemur.jumps()).toEqual(0);
+    });
+
+    it('Two alive branches', function() {
+        var lemur = new codeChallenges.LeapingLemur([0, 0]);
+        expect(lemur.jumps()).toEqual(1);
+    });
+
+    it('Three alive brances', function() {
+        var lemur = new codeChallenges.LeapingLemur([0, 0, 0]);
+        expect(lemur.jumps()).toEqual(1);
+    });
+
+    it('One Dead Branch', function() {
+        var lemur = new codeChallenges.LeapingLemur([0, 1, 0]);
+        expect(lemur.jumps()).toEqual(1);
+    });
+
+    it('One Dead Branch and Three alive Branches', function() {
+        var lemur = new codeChallenges.LeapingLemur([0, 0, 1, 0]);
+        expect(lemur.jumps()).toEqual(2);
+    });
+
+    it('Two Dead Branches', function() {
+        var lemur = new codeChallenges.LeapingLemur([0, 0, 0, 0, 1, 0, 0, 1, 0]);
+        expect(lemur.jumps()).toEqual(5);
     });
 
 });
